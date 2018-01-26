7dfc82796b901e1f9b0f54a156633ec078d9de7d
arturslataks <arturs@majaslapa.lv>
2018-01-26 18:17:06 +0200

    Updates to Author dashboard

--- src/app/js/components/pages/user/dashboard.vue
+++ src/app/js/components/pages/user/dashboard.vue
@@ -30,11 +30,29 @@
 
             <template v-else>
                 <div class="wrapper">
-                    <p>
-                        Welcome! Thank you for registration! Here you will soon see guidance videos on how to use the Publica platform. Stay tuned for updates!
-                    </p>
+                    <p class="welcome-msg">Listen to Publica guidance podcast by our CEO, Josef Marc</p>
 
-                    <iframe src="https://www.youtube.com/embed/zSUY_8MEikA?rel=0&amp;showinfo=0" class="dashboard-video" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <p v-if="error">{{ error }}</p>
+
+                    <ul class="accordion">
+                        <li v-for="(podcast, index) in podcasts" @click="podcast.expanded = !podcast.expanded" :key="index">
+                            <i></i>
+                            <p>{{ podcast.title }}</p>
+
+                            <transition name="expand" v-on:enter="togglePodcastEnter" v-on:leave="togglePodcastLeave" v-on:leave-cancelled="leaveCancelled" v-on:enter-cancelled="enterCancelled">
+                                <div class="item-content" v-show="podcast.expanded">
+                                    <audio ref="player" controls controlsList="nodownload">
+                                        <source v-bind:src="podcast.url">
+                                        Your browser does not support the audio element.
+                                    </audio>
+                                    <div class="actions">
+                                        <a href="#!">Download MP3</a>
+                                        <a href="#!">Show transcript</a>
+                                    </div>
+                                </div>
+                            </transition>
+                        </li>
+                    </ul>
                 </div>
             </template>
         </div>
@@ -46,12 +64,15 @@ import axios from 'axios';
 import pblUiFormField from 'components/ui/formField';
 import errorHandler from 'utils/errorHandler';
 import firebase from 'firebase';
+import Velocity from 'velocity-animate';
 
 export default {
     data() {
         return {
             error: false,
-            email: null
+            email: null,
+            podcasts: [],
+            animated: null,
         }
     },
 
@@ -74,11 +95,66 @@ export default {
                         this.error = error.message;
                     });
             }
+        },
+
+        fetchPodcasts() {
+            firebase.database().ref('/podcasts').once('value')
+            .then((podcasts) => {
+                podcasts = podcasts.val();
+
+                for (let podcast in podcasts) {
+                    podcasts[podcast].expanded = false;
+                }
+
+                this.podcasts = podcasts;
+            })
+            .catch((error) => {
+                this.error = 'Unable to fetch podcasts';
+                console.error(error);
+            });
+        },
+
+        enterCancelled(element) {
+            Velocity(element, 'stop');
+            this.togglePodcastLeave(element, () => {});
+        },
+
+        leaveCancelled(element) {
+            Velocity(element, 'stop');
+            this.togglePodcastEnter(element, () => {});
+        },
+
+        togglePodcastEnter(element, done) {
+            const height = element.offsetHeight + 'px';
+            const opacity = 1;
+            const translateY = '0'
+
+            element.style.position = 'relative';
+            element.style.height = 0;
+
+            Velocity(element, { height, opacity, translateY }, { complete: done });
+        },
+
+        togglePodcastLeave(element, done) {
+            const height = 0;
+            const translateY = '50%';
+            const opacity = 0;
+
+            Velocity(element, { height, translateY, opacity }, {
+                complete: () => {
+                    element.style.height = '';
+                    done();
+                }
+            });
         }
     },
 
     components: {
         pblUiFormField
+    },
+
+    created() {
+        this.fetchPodcasts();
     }
 }
 </script>
